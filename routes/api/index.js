e1cc976445acfb2c2e97546eb5e26bf0cd328881
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-01-11 21:33:14 -0800

    backend removed from app - unnecessary with Firebase

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const savedContactRoutes = require("./contacts");
-
-// Book routes
-router.use("/contacts", savedContactRoutes);
-
-module.exports = router;
--- routes/api/youtube.js
+++
@@ -1,9 +0,0 @@
-// const router = require("express").Router();
-// const contactsController = require("../../controllers/contactsController");
-
-// // Matches with "/api/books"
-// router.route("/")
-//     .get(contactsController.findAll)
-//     .post(contactsController.create);
-
-// module.exports = router;
