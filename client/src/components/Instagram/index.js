e6fb70c1b2ba229b14f0d258bc41c244807ba7fb
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-01-11 21:17:41 -0800

    build directory created

--- client/src/components/Instagram/index.js
+++ client/src/components/Instagram/index.js
@@ -1,17 +1,18 @@
-import { PromiseProvider } from "mongoose";
 import React from "react";
 import InstagramEmbed from 'react-instagram-embed';
 // import './style.css';
 
 function Instagram(props) {
+    // clientToken: '64c8065839fdf9286f3aaabc84727896';
+    // App-ID: '446941160007073'
+
     return (
         <div id="instagram">
             <h2> Latest Posts! </h2>
 
-            {/* Must use a Client Access Token */}
             <InstagramEmbed
-                url={props.instaUrl}
-                clientAccessToken='123|456'
+                url='https://www.instagram.com/p/CGXwxkUBKQB/'
+                clientAccessToken='446941160007073|64c8065839fdf9286f3aaabc84727896'
                 maxWidth={320}
                 hideCaption={false}
                 containerTagName='div'
