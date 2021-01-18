cfc7ef11351cde0a6310d57d7bfb86f2b1fc549d
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-01-18 13:07:19 -0800

    landing page updated with positioning and animations

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,19 +1,34 @@
 import React from "react";
 import "./landing.css";
 import { Link } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+import { fadeInRight, fadeInLeft } from 'react-animations';
 
 function Landing() {
+
+    // react-animations
+    const FadeInRight = styled.div`
+        animation: 2s ${keyframes`${fadeInRight}`}
+    `;
+    
+    const FadeInLeft = styled.div`
+        animation: 2s ${keyframes`${fadeInLeft}`}
+    `;
     return (
         <div className="landing" >
             <div className="container"> 
                 <div className="row">
-                    <h1 className="col-12" > Miracle Theory </h1>
-                    <p> fight or flight stories with some really cool people </p>
-                    <Link to="/home"className="nav-item nav-link" >
-                        <div className="bouncy" >
+                    <FadeInRight className="col-12"> 
+                        <h1 id="title"> Miracle Theory </h1>  
+                        <p id="slogan" className="col-12"> fight or flight stories with some really cool people </p>
+                    </FadeInRight>
+                    
+                    <FadeInLeft className="col-12" >
+                        <Link to="/home" className="nav-item nav-link buttonContainer" >
                             <button className="welcomeBtn" > Welcome </button>
-                        </div>
-                    </Link>
+                        </Link>
+                    </FadeInLeft>
+
                 </div>
             </div>
 
